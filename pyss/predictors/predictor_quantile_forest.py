4b9e651b92cfcb880f32cfc07283b84107e25f8b
minij147 <jakepaul628@gmail.com>
2025-09-04 14:35:44 -0400

    fixed long training time, cat issue in preprocess. Adding new bug with concating but it will be fixed

--- pyss/predictors/predictor_quantile_forest.py
+++ pyss/predictors/predictor_quantile_forest.py
@@ -1,6 +1,11 @@
 from .predictor import Predictor
 from pyss.base import job_types
 
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 from quantile_forest import RandomForestQuantileRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import Pipeline
@@ -12,6 +17,9 @@ from sklearn import feature_selection
 import pandas as pd
 import time
 
+import os
+
+WINDOW_MAX = 6000 #10000
 TRAIN_MAX = 5000
 RETRAIN_MAX = 100 # https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=7776517
 
@@ -19,16 +27,33 @@ RETRAIN_MAX = 100 # https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=777
 Each application gets there own Controller which wraps the necessary logic and functions
 """
 class Controller():
-    def __init__(self,quantiles): 
+    def __init__(self,job_type,quantiles,sliding_window=False,drop_cols=[]): 
         self.model = RandomForestQuantileRegressor()
         self.df = pd.DataFrame()
         self.train_batch = pd.DataFrame()
+        self.drop_cols = drop_cols
 
+        self.job_type = job_type
+        self.job_type_str = job_types.JOB_TYPE_TO_STR[job_type]
         self.is_trained = False
         self.quantiles = quantiles 
+        self.is_sliding_window = sliding_window
+
+        self.predicted_plot_x = []
+        self.predicted_plot_y = [[] for _ in range(len(quantiles))]
+        self.plot_iter = 0
+
+        self.timestamp_id = time.time()
+        os.makedirs(f"results/{self.timestamp_id}")
 
     def concat(self, new_df):
-        self.df = pd.concat([self.df, new_df], ignore_index=True)
+        # not sliding window or we aren't ready to trim the window
+        if not self.is_sliding_window or self.df.shape[0] < WINDOW_MAX:
+            self.df = pd.concat([self.df, new_df], ignore_index=True)
+            return
+        
+        # adjusting train window
+        self.df = pd.concat([self.df.iloc[RETRAIN_MAX:],new_df],ignore_index=True)
 
     def transform_df(self, predictCol):
         X = self.df
@@ -43,6 +68,14 @@ class Controller():
         X = X.replace('.false.', '0', regex=True)
 
         X = X.drop(columns=predictCol)
+
+        # drop cols
+        for col in self.drop_cols:
+            if not col in X.columns:
+                # print(f"warning {col} not in dataset, not dropping")
+                continue
+
+            X = X.drop(columns=col)
 
         return X,y
     
@@ -64,11 +97,11 @@ class Controller():
                 except:
                     # Otherwise, we will assume this is categorical data.
                     isNumeric = False
-            if isNumeric:
+            if isNumeric or X[col].dtype == float:
                 # For whatever reason, float conversions don't want to work in Pandas dataframes.
                 # Try changing the value column-wide instead.
                 # TODO: Doesn't seem to actually solve anything.
-                X.loc[col] = X.loc[col].astype(float)
+                # X.loc[col] = X.loc[col].astype(float)
                 numeric_features.append(str(col))
             else:
                 categorical_features.append(str(col))
@@ -111,20 +144,31 @@ class Controller():
                 pd.DataFrame([job.input_params])], 
                 ignore_index=True)
             return
+        
+        # plot logic
+        # if self.is_trained:
+            # self.plot()
 
-        print('starting train')
         # print(len(self.df))
         self.concat(self.train_batch)
 
         # temp code to verify dataframe
-        # self.df.to_csv(f"results/job-{job_types.JOB_TYPE_TO_STR[job.job_type]}-out.csv")
-
         X,y = self.transform_df(predictCol="timeTaken")
+        # X = X.to_numpy(dtype=np.float64)
+        # from threadpoolctl import threadpool_info
+        # print(threadpool_info())
+        # print(X.shape,y.shape)
+
+        # print("saved dataframe")
+        # X.to_csv(f"results/{self.timestamp_id}/job-{self.job_type_str}-out.csv")
         regressor = self.get_regressor(X)
+        # print(X.dtypes,regressor)
+        print("got regressor")
 
         startTime = time.process_time()
         self.regressor = regressor.fit(X, y)
         endTime = time.process_time()
+        print(self.job_type_str,'finsihed train on',self.df.shape[0],"data points - total time: ",endTime-startTime)
 
         # finishing the training
         self.is_trained = True
@@ -142,8 +186,37 @@ class Controller():
             exit()  
 
         x = pd.DataFrame([raw_job.input_params])
-        return self.regressor.predict(x,quantiles=self.quantiles)
-     
+        result = self.regressor.predict(x,quantiles=self.quantiles)
+        r = result[0]
+        for i in range(len(self.quantiles)):
+            self.predicted_plot_y[i].append(r[i])
+        self.predicted_plot_x.append(raw_job.actual_run_time)
+            
+        return result
+    
+    def plot(self):
+        colors = ['blue','green','red']
+        x = np.array(self.predicted_plot_x)
+        for i in range(len(self.quantiles)):
+            plt.scatter(
+                x,
+                np.array(self.predicted_plot_y[i]),
+                c=colors[i],
+                label=str(self.quantiles[i]),
+                alpha=0.7)
+        plt.scatter(x,x,c="black",label="actual_value",alpha=0.7)
+
+        title = self.job_type_str+" train_iter: "+str(self.plot_iter)
+        plt.xlabel("Actual Runtime")
+        plt.ylabel("Predicted Runtime")
+        plt.title(title)
+        plt.legend()
+        plt.savefig(f"results/{self.timestamp_id}/{title.replace(" ","-")}.png")
+        plt.close()
+
+        self.plot_iter+=1
+        
+
     @property
     def df_rows(self):
         return self.df.shape[0]
@@ -155,11 +228,40 @@ class PredictorQuantileForest(Predictor):
     def __init__(self, options):
         # do anything necessary that will require inits, 
         # so like creating the quantile forest model and selecting interval 
+        self.drop_cols = defaultdict(list)
+
+        self.drop_cols[job_types.JOB_TYPE_EXAMINIMDSNAP] = [ 
+            "testNum",
+            "units",
+            "lattice",
+            "lattice_constant",
+            "lattice_offset_x",
+            "lattice_offset_y",
+            "lattice_offset_z",
+            "lattice_ny",
+            "lattice_nz",
+            "ntypes",
+            "type",
+            "mass",
+            "force_cutoff",
+            "temperature_target",
+            "temperature_seed",
+            "neighbor_skin",
+            "comm_exchange_rate",
+            "thermo_rate",
+            "comm_newton",
+            "error"
+        ]
+
         self.controllers = {}
         self.train_batch = pd.DataFrame()
 
         for job_type in job_types.JOB_TYPE_RANGE:
-            self.controllers[job_type] = Controller(quantiles=[0.5,0.99])
+            self.controllers[job_type] = Controller(
+                job_type=job_type,
+                quantiles=[0.1,0.5,0.99],
+                sliding_window=True,
+                drop_cols=self.drop_cols[job_type])
 
         pass
     
@@ -169,16 +271,16 @@ class PredictorQuantileForest(Predictor):
         # maybe some methodogly could be used that if we our under a specfic runtime then we shouldn't predict
         controller = self.controllers[job.job_type]
 
-        if not controller.is_trained:
+        if not controller.is_trained or job.job_type == job_types.JOB_TYPE_NEKBONE:
             job.predicted_run_time = job.user_estimated_run_time
             return
         
         # print(controller.predict(job),job.actual_run_time)        
         result = controller.predict(job) # [[q1,q2,q3]]
         # print(result, type(result), type(result[0][0]))
-
-        q50 = int(result[0][0])
-        q99 = int(result[0][1])
+        # print(result[0],job.actual_run_time)
+        q50 = int(result[0][1])
+        q99 = int(result[0][2])
         if q50 > job.user_estimated_run_time:
             job.predicted_run_time = job.user_estimated_run_time
         else:
@@ -191,5 +293,8 @@ class PredictorQuantileForest(Predictor):
     For now we will just implement the intital train (5000 jobs) and worry about retraining later
     """
     def fit(self, job, current_time):
+        # temp debug
+        if job.job_type == job_types.JOB_TYPE_NEKBONE: 
+            return 
         controller = self.controllers[job.job_type]
         controller.fit(job) 
